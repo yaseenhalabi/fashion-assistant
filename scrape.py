d656cf5ee9f44af1a45c64a65bcce1233a60d96c
Yaseen Halabi <yaseenhalabi@gmail.com>
2023-10-09 01:08:00 -0500

    test

--- scrape.py
+++ scrape.py
@@ -13,7 +13,7 @@ import re
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36")
 driver = webdriver.Chrome(options=chrome_options)
 
 def main():
