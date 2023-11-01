0e797f8fd9b8a2cf0440fe02e2b2abea7fea1426
Yaseen Halabi <yaseenhalabi@gmail.com>
2023-11-01 10:23:45 -0500

    added product service to organize app.jsx

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -5,39 +5,27 @@ import Product from "./Product"
 import {useState, useEffect} from "react"
 import axios from "axios"
 import ReactLoading from 'react-loading' 
-import { fas } from "@fortawesome/free-solid-svg-icons"
+import { getProducts, getProductMatches } from "./services/ProductService"  
+
 function App() {
    
   const [productDataList, setProductDataList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [numItemsRequested, setNumItemsRequested] = useState(20)
-  //  api calls
-  const commenceScraping = async() => {
-    setIsLoading(true)
-    await axios.get(`http://localhost:8000/api/getAvailableProducts/` + numItemsRequested.toString())
-      .then(res => {
-        setProductDataList(res.data)
-        setProductDataList(list => list.map(item => ({...item, rating: "0"})));
 
-        setIsLoading(false)
-      }
-    )
+  const setProducts = async(num_of_items) => {
+    setIsLoading(true)
+    const data = await getProducts(num_of_items)
+    console.log(data)
+    setProductDataList(data)
+    setIsLoading(false)
   }
-  
-  const sendBackRatings = async() => {
-    try {
-      await axios.post("http://localhost:8000/api/findNewProducts/" + numItemsRequested.toString(), productDataList, 
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(res => {setProductDataList(res.data)})
-        
-    } catch (error) {
-      console.log(error)
-    }
-      
+
+
+  const setProductMatches = async(num_of_items) => {
+    const data = await getProductMatches(num_of_items, productDataList)
+    console.log(data) 
+    setProductDataList(data)         
   }
 
   // callback funciton that gets the star rating 
@@ -63,7 +51,7 @@ function App() {
   )
 
   //what user sees first
-  const startButton = <button className={"text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"} onClick={() => commenceScraping()}>Commence Scraping</button>
+  const startButton = <button className={"text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"} onClick={() => setProducts(2)}>Commence Scraping</button>
   const loadingElement = <ReactLoading className="m-auto mt-56" type="spinningBubbles" color="black" height={667} width={375} />
   const buttonThenLoading = isLoading? loadingElement : <div className="m-auto mt-56">{startButton}<p className="text-center">This may take a minute</p></div>
 
@@ -75,7 +63,7 @@ function App() {
 
     {cardElements}
 
-    {productDataList.length !== 0? <button onClick={() => sendBackRatings()} className="bg-blue-500 text-white hover:bg-blue-700 py-2 px-4 font-bold rounded-lg m-auto">Submit</button>: ""}
+    {productDataList.length !== 0? <button onClick={() => setProductMatches(2)} className="bg-blue-500 text-white hover:bg-blue-700 py-2 px-4 font-bold rounded-lg m-auto">Submit</button>: ""}
 
     </div>
 
---
+++ frontend/app/src/services/ProductService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const getProducts = async(num_of_items) => {
+  var data
+  
+  await axios.get(`http://localhost:8000/api/getAvailableProducts/` + num_of_items.toString())
+    .then(response => {
+      data = response.data
+      data = data.map(item => ({...item, rating: "0"}))
+      console.log(data)
+    })
+  return data
+}
+
+export const getProductMatches = async(num_of_items, rated_products) => {
+    
+  var data
+
+  try {
+    await axios.post("http://localhost:8000/api/findNewProducts/" + num_of_items.toString(), rated_products, 
+      {
+        headers: {'Content-Type': 'application/json'}
+      }
+    ).then(response => {data = response.data})
+  
+  } catch (error) {
+    console.log(error)
+  }
+  
+  return data
+}
+
+
