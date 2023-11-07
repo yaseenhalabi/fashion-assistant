d57469233ff7a21da57ca6482efd69d19bcfc213
Yaseen Halabi <yaseenhalabi@gmail.com>
2023-11-07 13:23:47 -0600

    added products to product container

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,1 +1,32 @@
-// place files you want to import through the `$lib` alias in this folder.
+import axios from "axios";
+
+export const getProducts = async(num_of_items) => {
+  var data
+  
+  await axios.get(`http://localhost:8000/api/getProducts/` + num_of_items.toString())
+    .then(response => {
+      data = response.data
+      data = data.map(item => ({...item, rating: "0"}))
+    })
+  return data
+}
+
+export const getProductMatches = async(num_of_items, rated_products) => {
+    
+  var data
+
+  try {
+    await axios.post("http://localhost:8000/api/getProductMatches/" + num_of_items.toString(), rated_products, 
+      {
+        headers: {'Content-Type': 'application/json'}
+      }
+    ).then(response => {data = response.data})
+  
+  } catch (error) {
+    console.log(error)
+  }
+  
+  return data
+}
+
+
--- frontend/src/routes/ProductContainer.svelte
+++ frontend/src/routes/ProductContainer.svelte
@@ -1,7 +1,31 @@
 <script>
   import Product from "../components/Product.svelte"
+  import { getProducts, getProductMatches } from "../api/index.js"
+  import { onMount } from 'svelte';
+  
+  let products = []
+
+  onMount(async () => {
+    products = await getProducts(10); // Load 10 products on component mount, for example
+    console.log(products)
+  });
+
+
 </script>
 
 <div>
-  <Product />
+
+  {#each products as product (product.url)} <!-- Assuming each product has a unique 'id' -->
+    <Product 
+      imageAddress={product.image} 
+      url={product.url} 
+      tags={product.tags} 
+      price={product.price}
+      size={product.size}
+      color={product.color}
+      condition={product.condition}
+      description={product.description}
+    />
+  {/each}
+
 </div>
