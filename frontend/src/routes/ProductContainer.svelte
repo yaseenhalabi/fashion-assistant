cd51ba592cc5baee8c42479b0ef4b0cdb3dda8f3
Yaseen Halabi <yaseenhalabi@gmail.com>
2023-11-07 19:40:37 -0600

    added find matches functionality to svelte frontend

--- frontend/src/routes/ProductContainer.svelte
+++ frontend/src/routes/ProductContainer.svelte
@@ -9,12 +9,15 @@
     products = await getProducts(10); // Load 10 products on component mount, for example
   });
 
-
+  const fetchProductMatches = async() => {
+    products = await getProductMatches(10, products)
+  }
+  
 </script>
 
-<div class="">
+<div>
   <div class="grid md:grid-cols-2 gap-4">
-  {#each products as product (product.url)} <!-- Assuming each product has a unique 'id' -->
+  {#each products as product (product.url)}
     <Product 
       imageAddress={product.image} 
       url={product.url} 
@@ -27,5 +30,9 @@
     />
   {/each}
   </div>
-  <button>hello</button>
+  <button 
+    on:click={fetchProductMatches} 
+    class="bg-blue-500 text-white hover:bg-blue-700 py-2 w-full font-bold my-8">
+    Find Matches
+  </button>
 </div>
