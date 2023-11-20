ec8a15f9504372282aadb15f121c20d9db8bec48
Yaseen Halabi <yaseenhalabi@gmail.com>
2023-11-19 18:49:28 -0600

    made small change

--- backend/match.py
+++ backend/match.py
@@ -24,8 +24,8 @@ def meetsPreferences(product, preferences):
 
 
 def sortByRelevance(available_products, selected_images):
-    sorted_products = []
-    return sorted_products
+    # insert code here
+    return []
 
 def getMatches(selected_images, available_products, preferences):
 
@@ -42,4 +42,10 @@ def getMatches(selected_images, available_products, preferences):
     return list_of_matching_products[:preferences['num_of_items']]
 
 
+import sample_data
 
+sample_products = sample_data.getData()
+sample_images = ["https://media-assets.grailed.com/prd/listing/44972277/944c4e0bc87743088c6b3f68b3150b77?w=1050&h=1400&fit=clip&q=20&auto=format"]
+
+if __name__ == "main":
+    sortByRelevance(sample_products, sample_images)
